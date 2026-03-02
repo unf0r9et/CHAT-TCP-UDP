2bf02b9bff1abea51ced174c3c60def5f995249d
unf0r9et <unforgettableunf2006@gmail.com>
2026-03-02 21:28:55 +0300

    START: BASE

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	myApp := app.NewWithID("com.unf0r9et.chat")
+
+	myWindow := myApp.NewWindow("CHAT-TCP-UDP")
+
+	myWindow.Resize(fyne.NewSize(200,200))
+
+	myWindow.ShowAndRun()
+}
